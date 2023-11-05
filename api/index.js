6bb34fffbab901bf71ca918b3013fd88c96a7325
PrashantLalwani8289 <prashantlalwani8289@gmail.com>
2023-11-05 06:57:26 +0530

    "created the Database and Mongoose model for user"

--- api/index.js
+++ api/index.js
@@ -1,4 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log("connected to database");
+}).catch((err) => {
+    console.log(err)
+})
 
 const app = express();
 
